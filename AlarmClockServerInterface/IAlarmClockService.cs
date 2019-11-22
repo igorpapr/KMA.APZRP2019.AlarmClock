8341a4915622b11684879f6693b04f3d92a90395
Ihor Paprotskyi <igorpapr@gmail.com>
2019-11-23 01:36:40 +0200

    Server interface, service impl added

---
+++ AlarmClockServerInterface/IAlarmClockService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using KMA.APZRP2019.AlarmClock.DBModels;
+
+namespace KMA.APZRP2019.AlarmClock.Server.Interface
+{
+    [ServiceContract]
+    public interface IAlarmClockService
+    {
+        [OperationContract]
+        IEnumerable<User> GetAllUsers();
+
+        [OperationContract]
+        void AddUser(User user);
+    }
+}
---
+++ AlarmClockServiceImpl/AlarmClockImpl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using KMA.APZRP2019.AlarmClock.Server.Interface;
+using KMA.APZRP2019.AlarmClock.DBModels;
+using KMA.APZRP2019.AlarmClock.EntityFrameworkWrapper;
+
+namespace KMA.APZRP2019.AlarmClock.Server.AlarmClockServiceImpl
+{
+    public class AlarmClockImpl : IAlarmClockService
+    {
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var context = new AlarmClockDbContext())
+            {
+                return context.Users.Include(u => u.AlarmClocks).ToList();
+            }
+        }
+
+        public void AddUser(User user)
+        {
+            using (var context = new AlarmClockDbContext())
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+        }
+    }
+}
