7a33dbf802925ba60f36981ff8f0f0aa1680e041
Ihor Paprotskyi <igorpapr@gmail.com>
2019-11-22 17:21:51 +0200

    recreated projects

---
+++ DBModels/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DBModels
+{
+    [DataContract(IsReference = true)]
+
+    class User : IDBModel
+    {
+
+    }
+}
