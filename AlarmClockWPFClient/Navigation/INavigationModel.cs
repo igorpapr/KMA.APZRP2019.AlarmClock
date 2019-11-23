6e38cc02b20a40fcbf1a9ed1c4a2cd4b7800ae7a
Dan <lyash.dv@gmail.com>
2019-11-23 22:43:52 +0200

    Nav

---
+++ AlarmClockWPFClient/Navigation/INavigationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClockWPFClient.Navigation
+{
+    internal enum ViewType
+    {
+        SignIn,
+        SignUp,
+        Main
+    }
+
+    interface INavigationModel
+    {
+        void Navigate(ViewType viewType);
+    }
+}
